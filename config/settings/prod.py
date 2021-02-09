1512c46f43054ac3c9145e202b99f2f0d0be8599
최웅빈 <woongbin96@naver.com>
2021-02-10 00:43:49 +0900

    변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,3 +7,14 @@ STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '!1samyang',
+        'HOST': 'ls-266a09787bb1193fb57f62b53100c40eab6a75bf.crbe8p6vmk6u.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
